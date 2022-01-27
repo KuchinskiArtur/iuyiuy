8e8f9a3955f19f0bbb7bb295b55663df1cd6e3e2
Artur <vasya.rogin@inbox.ru>
2022-01-27 20:44:34 +0300

    redone-2

--- script.js
+++ script.js
@@ -42,15 +42,9 @@ let patronymic = getValue('patronymic');
 let age = getValue('age');
 let gender = getValue('gender');
 
-alert(` 
-    Ваше ФИО: ${names} ${surname} ${patronymic} \n
-    Ваш возраст в годах: ${age} \n 
-    Ваш возраст в днях: ${age * 365} \n 
-    Через 5 лет вам будет: ${age + 5} \n
-    Ваш пол: ${gender} \n
-    Вы на пенсии: ${(gender == false && age >= 55) || (gender && age >= 65) ? 'да' : 'нет'}
-    `)
-
+alert(` Ваше ФИО: ${names} ${surname} ${patronymic} \n Ваш возраст в годах: ${age} \n 
+        Ваш возраст в днях: ${age * 365} \n Через 5 лет вам будет: ${age + 5} \n
+        Ваш пол: ${gender} \n Вы на пенсии: ${(gender == false && age >= 55) || (gender && age >= 65) ? 'да' : 'нет'} `)
 
 function getCount(str) {
     let vowelsCount = 0;
